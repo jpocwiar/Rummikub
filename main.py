286809ae044b7287555a1e354cf4cd672ab26b4b
Jakub Poćwiardowski <80002864+Pocwiardo@users.noreply.github.com>
2023-03-19 20:58:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from PySide2.QtWidgets import QApplication, QGraphicsScene, QGraphicsView, QGraphicsItem
-from PySide2.QtGui import QColor, QBrush, QPen
-from PySide2.QtCore import Qt, QRectF
+from PySide2.QtGui import QColor, QBrush, QPen, QFont, QPainter
+from PySide2.QtCore import Qt, QRectF, QPointF
+import random
 
-class Klocek(QGraphicsItem):
-    def __init__(self, kolor, numer):
+class Tile(QGraphicsItem):
+    def __init__(self, colour, numer):
         super().__init__()
-        self.rect = QRectF(0, 0, 50, 80)  # Ustawienie wymiarów klocka
-        self.kolor = kolor  # Kolor klocka
+        self.width = 50
+        self.height = 80
+        self.rect = QRectF(0, 0, self.width, self.height)  # Ustawienie wymiarów klocka
+        self.colour = colour  # colour klocka
         self.numer = numer  # Numer klocka
 
     def boundingRect(self):
@@ -14,52 +17,93 @@ class Klocek(QGraphicsItem):
 
     def paint(self, painter, option, widget):
         # Rysowanie klocka
-        painter.setBrush(QBrush(self.kolor))
+        painter.setBrush(QBrush(QColor(255, 249, 213)))
         painter.setPen(QPen(Qt.black))
         painter.drawRect(self.rect)
 
-class Plansza(QGraphicsScene):
+        # Rysowanie numeru klocka
+        painter.setFont(QFont('Arial', 16))
+        numer_text = str(self.numer)
+        numer_rect = painter.fontMetrics().boundingRect(numer_text)
+        numer_x = self.rect.center().x() - numer_rect.width() / 2
+        numer_y = self.rect.center().y() - numer_rect.height() / 4
+        painter.setPen(QPen(self.colour))
+        painter.drawText(QPointF(numer_x, numer_y), numer_text)
+
+class ForegroundItem(QGraphicsItem):
+    def __init__(self, width, height):
+        super().__init__()
+        self.width = width
+        self.height = height
+        self.rect = QRectF(0, 0, self.width, self.height)
+
+    def boundingRect(self):
+        return self.rect
+
+    def paint(self, painter, option, widget):
+        painter.setBrush(QBrush(QColor(255, 255, 255, 0))) # Set the brush color to transparent
+        painter.drawRect(self.rect)
+
+class Board(QGraphicsScene):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.setSceneRect(0, 0, 500, 500)  # Ustawienie wymiarów planszy
-        self.setBackgroundBrush(QBrush(QColor(238, 238, 238)))  # Ustawienie tła planszy
-        self.klocki = []  # Lista klocków
+        self.setSceneRect(0, 0, 1800, 1000)
+        self.setBackgroundBrush(QBrush(QColor(238, 238, 238)))
+        self.tiles = []
+        self.width = 50
+        self.height = 80
+        self.foreground_item = ForegroundItem(self.width * 10, self.height * 2) # Create an instance of ForegroundItem
+        self.foreground_item.setPos(self.sceneRect().width() / 2 - self.width * 10 / 2, self.sceneRect().height() - self.height * 2)
+        self.addItem(self.foreground_item) # Add the foreground item to the scene
 
-    def generuj_klocki(self):
+
+    def generate_tiles(self):
         # Generowanie klocków
-        kolory = [Qt.red, Qt.blue, Qt.yellow, Qt.green, Qt.black, Qt.magenta, Qt.cyan]
-        for kolor in kolory:
+        colours = [Qt.red, Qt.blue, Qt.yellow, Qt.black]
+        for colour in colours:
             for numer in range(1, 14):
-                klocek = Klocek(kolor, numer)
-                klocek.setFlag(QGraphicsItem.ItemIsMovable)  # Ustawienie możliwości przesuwania klocka
-                self.addItem(klocek)
-                self.klocki.append(klocek)
+                for i in range(2):
+                    tile = Tile(colour, numer)
+                    tile.setFlag(QGraphicsItem.ItemIsMovable)  # Ustawienie możliwości przesuwania klocka
+                    self.addItem(tile)
+                    self.tiles.append(tile)
+        random.shuffle(self.tiles)
+
+    def snap_to_grid(self, pos):
+        # Obliczenie pozycji klocka na siatce
+        x = int(round(pos.x() / self.width)) * self.width
+        y = int(round(pos.y() / self.height)) * self.height
+        return QPointF(x, y)
+
 
     def mousePressEvent(self, event):
         # Znajdowanie klocka, który został kliknięty
-        for klocek in self.klocki:
-            if klocek.boundingRect().contains(event.scenePos()):
-                self.drag_klocek = klocek
+        items = self.items(event.scenePos())
+        for item in items:
+            if isinstance(item, Tile):
+                self.drag_tile = item
                 break
+
 
     def mouseMoveEvent(self, event):
         # Przeciąganie klocka
-        if hasattr(self, 'drag_klocek'):
-            pos = event.scenePos() - self.drag_klocek.boundingRect().center()
-            self.drag_klocek.setPos(pos)
+        if hasattr(self, 'drag_tile') and self.drag_tile is not None:
+            pos = event.scenePos() - self.drag_tile.boundingRect().center()
+            self.drag_tile.setPos(pos)
 
     def mouseReleaseEvent(self, event):
-        # Usuwanie klocka, jeśli zostanie upuszczony poza planszą
-        if hasattr(self, 'drag_klocek'):
-            if not self.sceneRect().contains(self.drag_klocek.sceneBoundingRect()):
-                self.removeItem(self.drag_klocek)
-                self.klocki.remove(self.drag_klocek)
+        # Move the dropped tile to the closest grid position
+        if hasattr(self, 'drag_tile') and self.drag_tile is not None:
+            pos = self.snap_to_grid(self.drag_tile.pos())
+            self.drag_tile.setPos(pos)
+            self.drag_tile = None
 
 if __name__ == '__main__':
     app = QApplication([])
     view = QGraphicsView()
-    plansza = Plansza()
-    plansza.generuj_klocki()
-    view.setScene(plansza)
+    board = Board()
+
+    view.setScene(board)
+    board.generate_tiles()
     view.show()
     app.exec_()
